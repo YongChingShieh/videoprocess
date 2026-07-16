47e7c03044705cb82d764be415e9df1031bb8f8d
YongChing <ourhonour2008@gmail.com>
2026-07-16 20:42:20 +0800

    remove  Microsoft.AspNetCore.OpenApi

--- Program.cs
+++ Program.cs
@@ -23,11 +23,6 @@ builder.WebHost.ConfigureKestrel(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
  
 
